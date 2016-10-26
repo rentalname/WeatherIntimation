ef8b843f05157ecf51d674b283328e1bd28b07bd
rentalname <agenda21st@gmail.com>
2016-10-26 18:20:00 +0900

    init

---
+++ app/src/main/java/liberr/net/weatherintimation/MainActivity.kt
@@ -0,0 +1,12 @@
+package liberr.net.weatherintimation
+
+import android.support.v7.app.AppCompatActivity
+import android.os.Bundle
+
+class MainActivity : AppCompatActivity() {
+
+    protected fun onCreate(savedInstanceState: Bundle) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_main)
+    }
+}
